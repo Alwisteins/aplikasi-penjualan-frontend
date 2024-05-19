c1c986827ae12dd2d25576790532fc5504163efc
unknown <alwyyourbae@gmail.com>
2024-05-20 00:32:25 +0700

    feat: add search page

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Create from "./pages/create/Create";
 import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/tambah-barang" element={<Create />} />
+      <Route path="/cari-barang" element={<Search />} />
     </Routes>
   );
 }
