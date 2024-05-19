f89d11a158238eeb0ec3d6cbd7ea350aeddb7c92
unknown <alwyyourbae@gmail.com>
2024-05-20 00:34:42 +0700

    feat: add FilterButton

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,6 +1,41 @@
 import { useState } from "react";
 import axios from "axios";
-import { ModalForm } from "../modal/Modal";
+import { ModalForm, ModalFilter } from "../modal/Modal";
+
+export function OptionButton({ name }) {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <button
+      className={`${
+        isActive
+          ? "bg-green-200 border-green-400"
+          : "bg-slate-100 border-slate-300"
+      } border py-1 px-4 rounded-xl`}
+      onClick={() => setIsActive(!isActive)}
+    >
+      {name}
+    </button>
+  );
+}
+
+export function FilterButton() {
+  const [openModal, setOpenModal] = useState(false);
+
+  const onCloseModal = () => {
+    setOpenModal(false);
+  };
+  return (
+    <>
+      <button
+        className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+        onClick={() => setOpenModal(true)}
+      >
+        Filter
+      </button>
+      <ModalFilter openModal={openModal} onCloseModal={onCloseModal} />
+    </>
+  );
+}
 
 export function AddButton() {
   const [openModal, setOpenModal] = useState(false);
